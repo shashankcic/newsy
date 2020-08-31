a02e4a7ca9b08f1e1a6a8dfee074f6da61005dc0
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-08-31 18:24:26 +0530

    Updates

---
+++ static/js/53.d705e626.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpNewsy=this.webpackJsonpNewsy||[]).push([[53],{177:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(6),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.j)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.d705e626.chunk.js.map
